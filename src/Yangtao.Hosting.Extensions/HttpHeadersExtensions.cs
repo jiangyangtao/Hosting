30b40ab3e90048b5e958def1c1225bbf1e75668b
Yangtao Jiang <jiangyangtao1@qq.com>
2022-12-05 02:57:22 +0800

    Add HttpHeaders the extensions

---
+++ src/Yangtao.Hosting.Extensions/HttpHeadersExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yangtao.Hosting.Extensions
+{
+    public static class HttpHeadersExtensions
+    {
+
+        /// <summary>
+        /// 获取当前请求中指定 Header 的值
+        /// </summary>
+        /// <param name="request">HttpRequest 对象</param>
+        /// <param name="key">获取 Headers 值对应的 key</param>
+        /// <returns>
+        /// 如果 request 对象为 null，则返回 <see cref="string.Empty"/>;
+        /// 如果 key 参数为 null 或空字符串 ("")，则返回 <see cref="string.Empty"/>;
+        /// 如果 <see cref="IHeaderDictionary"/> 中不包含指定的 key，则返回 <see cref="string.Empty"/>;
+        /// 否则返回 <see cref="IHeaderDictionary"/> 中指定 key 对应的 value 。
+        /// </returns>
+        public static string GetValue(this IHeaderDictionary headers, string key)
+        {
+            if (headers == null) return string.Empty;
+            if (key.IsNullOrEmpty()) return string.Empty;
+            if (headers.ContainsKey(key) == false) return string.Empty;
+
+            return headers[key].ToString();
+        }
+    }
+}
--- src/Yangtao.Hosting.Extensions/HttpRequestExtensions.cs
+++ src/Yangtao.Hosting.Extensions/HttpRequestExtensions.cs
@@ -35,24 +35,5 @@ namespace Yangtao.Hosting.Extensions
                 .ToString();
         }
 
-        /// <summary>
-        /// 获取当前请求 Header 的值
-        /// </summary>
-        /// <param name="request">HttpRequest 对象</param>
-        /// <param name="key">获取 Headers 值对应的 key</param>
-        /// <returns>
-        /// 如果 request 对象为 null，则返回 <see cref="string.Empty"/>;
-        /// 如果 key 参数为 null 或空字符串 ("")，则返回 <see cref="string.Empty"/>;
-        /// 如果 <see cref="HttpRequest.Headers"/> 中不包含指定的 key，则返回 <see cref="string.Empty"/>;
-        /// 否则返回 <see cref="HttpRequest.Headers"/> 中指定 key 对应的 value 。
-        /// </returns>
-        public static string GetValue(this HttpRequest request, string key)
-        {
-            if (request == null) return string.Empty;
-            if (key.IsNullOrEmpty()) return string.Empty;
-            if (request.Headers.ContainsKey(key) == false) return string.Empty;
-
-            return request.Headers[key].ToString();
-        }
     }
 }
