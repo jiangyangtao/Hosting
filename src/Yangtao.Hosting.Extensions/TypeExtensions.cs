f828273fb903afcfabd0b6e77776d95be60744e5
Yangtao Jiang <jiangyangtao1@qq.com>
2022-12-07 20:49:44 +0800

    Add Type the extension method

--- src/Yangtao.Hosting.Extensions/TypeExtensions.cs
+++ src/Yangtao.Hosting.Extensions/TypeExtensions.cs
@@ -15,5 +15,28 @@ namespace Yangtao.Hosting.Extensions
 
             return type.FullName;
         }
+
+        public static bool HasInterface(this Type type, string name)
+        {
+            if (name.IsNullOrEmpty()) return false;
+
+            var interfaceType = type.GetInterface(name);
+            return interfaceType != null;
+        }
+
+        public static bool HasInterface(this Type type, Type interfaceType)
+        {
+            if (type == null) return false;
+            if (interfaceType == null) return false;
+
+            return type.GetInterfaces().Any(a => a == interfaceType);
+        }
+
+        public static bool HasInterface<T>(this Type type)
+        {
+            if (type == null) return false;
+
+            return HasInterface(type, typeof(T));
+        }
     }
 }
