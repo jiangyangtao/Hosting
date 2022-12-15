9720a071936f6818e769f3d4c88deedf2680bb92
yangtaojiang <yangtaojiang@scienbizip.com>
2022-12-15 14:21:02 +0800

    Adjustment result and add interceptor

---
+++ src/Yangtao.Hosting.GrpcServer/Interceptors/GrpcGlobalInterceptor.cs
@@ -0,0 +1,69 @@
+﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yangtao.Hosting.Core.HttpErrorResult;
+
+namespace Yangtao.Hosting.GrpcServer.Interceptors
+{
+    internal class GrpcGlobalInterceptor : Interceptor
+    {
+        public GrpcGlobalInterceptor()
+        {
+        }
+
+        public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(TRequest request, ServerCallContext context, UnaryServerMethod<TRequest, TResponse> continuation)
+        {
+            try
+            {
+                return await continuation(request, context);
+            }
+            catch (Exception e)
+            {
+                var (code, message) = GetErrorResult(e);
+                var response = CreateErrorResponse(continuation, code, message);
+                if (response == null) throw new RpcException(Status.DefaultCancelled, e.Message);
+
+                return (TResponse)response;
+            }
+
+        }
+
+        private static (int code, string message) GetErrorResult(Exception exception)
+        {
+            var code = -1;
+            var message = exception.Message;
+            if (exception is HttpErrorResult httpError)
+            {
+                var result = httpError.GetErrorResult();
+                code = result.Code;
+                message = result.Message;
+            }
+
+            return (code, message);
+        }
+
+        private static object CreateErrorResponse<TRequest, TResponse>(UnaryServerMethod<TRequest, TResponse> continuation, int code, string message) where TRequest : class where TResponse : class
+        {
+            var responseType = continuation.Target.GetType().GenericTypeArguments.LastOrDefault();
+            var response = Activator.CreateInstance(responseType);
+            var propertyInfos = responseType.GetProperties();
+            var errorResultType = propertyInfos.FirstOrDefault(a => a.Name.Contains("Error"));
+            if (errorResultType == null) return null;
+
+            var errorResult = Activator.CreateInstance(errorResultType.PropertyType);
+            var errorPropertyInfos = errorResultType.PropertyType.GetProperties();
+            foreach (var item in errorPropertyInfos)
+            {
+                if (item.Name == "Code") item.SetValue(errorResult, code, null);
+                if (item.Name == "Message") item.SetValue(errorResult, message, null);
+            }
+            errorResultType.SetValue(response, errorResult, null);
+
+            return response;
+        }
+    }
+}
--- src/Yangtao.Hosting.GrpcServer/ServiceCollectionExtensions.cs
+++ src/Yangtao.Hosting.GrpcServer/ServiceCollectionExtensions.cs
@@ -1,7 +1,20 @@
-﻿namespace Yangtao.Hosting.GrpcServer
+﻿using Grpc.AspNetCore.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Yangtao.Hosting.GrpcServer.Interceptors;
+
+namespace Yangtao.Hosting.GrpcServer
 {
     public static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddGrpcServer(this ServiceCollection services, Action<GrpcServiceOptions> action)
+        {
+            services.AddGrpc(options =>
+            {
+                options.Interceptors.Add<GrpcGlobalInterceptor>();
+                action(options);
+            });
 
+            return services;
+        }
     }
 }
