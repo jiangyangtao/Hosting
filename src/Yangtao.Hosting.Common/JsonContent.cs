daf6da0a39b59eed3499bd7c4e104d6657bb585d
jiangyangtao <yangtaojiang@scienbizip.com>
2022-12-16 11:24:57 +0800

    Add JsonContent

---
+++ src/Yangtao.Hosting.Common/JsonContent.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System.Net.Mime;
+using System.Text;
+
+namespace Yangtao.Hosting.Common
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object data) : base(JsonConvert.SerializeObject(data), Encoding.UTF8, MediaTypeNames.Application.Json)
+        { }
+    }
+
+    public class JsonContent<TData> : StringContent where TData : class
+    {
+        public JsonContent(TData data) : base(JsonConvert.SerializeObject(data), Encoding.UTF8, MediaTypeNames.Application.Json)
+        { }
+    }
+}
