c769a6d1ea46ab46e7037c6047d4e68e346cd652
Yangtao Jiang <jiangyangtao1@qq.com>
2022-12-22 01:49:55 +0800

    Update swagger

--- src/Yangtao.Hosting.Swagger/ServiceCollectionExtensions.cs
+++ src/Yangtao.Hosting.Swagger/ServiceCollectionExtensions.cs
@@ -1,22 +1,55 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using Yangtao.Hosting.Extensions;
+using Yangtao.Hosting.Swagger.Filters;
 
 namespace Yangtao.Hosting.Swagger
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        public static IServiceCollection AddSwagger(this IServiceCollection services, Action<SwaggerOptions> action)
         {
+            var swaggerOptions = new SwaggerOptions();
             services.AddSwaggerGen(options =>
             {
-                options.SwaggerDoc("", new OpenApiInfo
+
+                options.OrderActionsBy(o => o.RelativePath);     
+                var referencedAssemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+                referencedAssemblies.ToList().ForEach(assembly =>
                 {
-                    //Version = version,
-                    //Title = $"{ApiName} 接口文档——{RuntimeInformation.FrameworkDescription}",
-                    //Description = $"{ApiName} HTTP API " + version,
+                    var path = Path.Combine(AppContext.BaseDirectory, $"{assembly.Name}.xml");
+                    if (File.Exists(path)) options.IncludeXmlComments(path);
                 });
-                options.OrderActionsBy(o => o.RelativePath);
+
+                var xmlFileName = swaggerOptions.ProjectXmlFileName.NotNullAndEmpty() ? swaggerOptions.ProjectXmlFileName : $"{swaggerOptions.ProjectName}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFileName);
+                if (File.Exists(xmlPath)) options.IncludeXmlComments(xmlPath);
+
+                var doc = XDocument.Load(xmlPath);
+                options.OperationFilter<SwaggerOperationFilter>(doc);
+                options.SchemaFilter<SwaggerSchemaFilter>();
+                options.DocumentFilter<SwaggerDocumentFilter>();
+
+
+
+
+
+
+
+
+
+
+
+                swaggerOptions.SwaggerGenOptions = options;
+                action?.Invoke(swaggerOptions);
+
+                options.SwaggerDoc(swaggerOptions.ProjectName, swaggerOptions.OpenApiInfo);
+
             });
             return services;
         }
---
+++ src/Yangtao.Hosting.Swagger/SwaggerOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yangtao.Hosting.Swagger
+{
+    public class SwaggerOptions
+    {
+        public string ProjectName { set; get; }
+
+        public string ProjectXmlFileName { set; get; }
+
+        public OpenApiInfo OpenApiInfo { set; get; }
+
+        public SwaggerGenOptions SwaggerGenOptions { set; get; }
+    }
+}
