d1498905aefdd9c0453fb1f1e90dffef2275c370
Yangtao Jiang <jiangyangtao1@qq.com>
2023-02-10 15:24:27 +0800

    Add custom configure the kestrel

---
+++ src/Yangtao.Hosting.Core/KestrelConfigure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yangtao.Hosting.Core
+{
+    public class KestrelConfigure
+    {
+        public Endpoint[] Endpoints { set; get; }
+    }
+
+    public class Endpoint
+    {
+        public int Port { set; get; }
+
+        public Certificate Certificate { set; get; }
+    }
+
+    public class Certificate
+    {
+        public string Path { set; get; }
+
+        public string FileName { set; get; }
+
+        public string Password { set; get; }
+    }
+}
---
+++ src/Yangtao.Hosting.Core/WebHostKestrel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Security.Authentication;
+
+namespace Yangtao.Hosting.Core
+{
+    public static class WebHostKestrel
+    {
+        public static void CustomConfigureKestrel(KestrelServerOptions serverOptions, IConfigurationSection configuration)
+        {
+            var kestrelConfigure = configuration.Get<KestrelConfigure>();
+            if (kestrelConfigure != null)
+            {
+                foreach (var endpoint in kestrelConfigure.Endpoints)
+                {
+                    Action<ListenOptions> action = (listenOptions) => { };
+                    if (endpoint.Certificate != null)
+                    {
+                        action = (listenOptions) =>
+                        {
+                            var basePath = Directory.GetCurrentDirectory();
+                            var certPath = Path.Combine(basePath!, endpoint.Certificate.Path, endpoint.Certificate.FileName);
+                            listenOptions.UseHttps(certPath, endpoint.Certificate.Password, adapterOptions => adapterOptions.SslProtocols = SslProtocols.Tls13);
+                        };
+                    }
+
+                    serverOptions.Listen(IPAddress.Loopback, endpoint.Port, action);
+                }
+            }
+        }
+    }
+}
