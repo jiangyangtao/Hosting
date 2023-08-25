63c9f28b6082f673f27f1ce2975b4b447456ec02
Yangtao Jiang <jiangyangtao1@qq.com>
2023-08-26 03:25:41 +0800

    Add EnumConfiguration

---
+++ src/Yangtao.Hosting.Controller/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Yangtao.Hosting.Controller.EnumConfiguration;
+
+namespace Yangtao.Hosting.Controller
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        public static IEndpointRouteBuilder UseEnumConfiguration(this IEndpointRouteBuilder endpointRouteBuilder, Action<EnumConfigurationOptions> optionAction)
+        {
+            var options = new EnumConfigurationOptions();
+            optionAction(options);
+
+            var enumConfiguration = new EnumConfigurationBuilder(options).GetEnumConfiguration();
+            var routeHandlerBuilder = endpointRouteBuilder.MapGet(options.EnumConfigurationEndpoint, async (HttpResponse response) =>
+            {
+                response.ContentType = options.ResponseContentType;
+                await response.WriteAsync(enumConfiguration);
+            });
+
+            if (options.RequireAuthorization) routeHandlerBuilder.RequireAuthorization();
+            return endpointRouteBuilder;
+        }
+    }
+}
---
+++ src/Yangtao.Hosting.Controller/EnumConfiguration/EnumConfigurationOptions.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mime;
+
+namespace Yangtao.Hosting.Controller.EnumConfiguration
+{
+    public class EnumConfigurationOptions
+    {
+        public EnumConfigurationOptions()
+        {
+        }
+
+        /// <summary>
+        /// Get EnumConfiguration the endpoint,Default "/EnumConfiguration"
+        /// </summary>
+        public string EnumConfigurationEndpoint { set; get; } = "/EnumConfiguration";
+
+        public bool RequireAuthorization { set; get; } = true;
+
+        public bool IncludeNumericField { set; get; } = false;
+
+        /// <summary>
+        /// Enum the Name Attribute,Default "Text"
+        /// </summary>
+        public string EnumNameField { set; get; } = "Text";
+
+        /// <summary>
+        /// Enum the Value,Default "Value"
+        /// </summary>
+        public string EnumValueField { set; get; } = "Value";
+
+        /// <summary>
+        /// Enum the Number,Default "Number"
+        /// </summary>
+        public string EnumNumericField { set; get; } = "Number";
+
+        /// <summary>
+        /// Enum Result the ContentType,Default application/json
+        /// </summary>
+        public string ResponseContentType { set; get; } = MediaTypeNames.Application.Json;
+    }
+}
