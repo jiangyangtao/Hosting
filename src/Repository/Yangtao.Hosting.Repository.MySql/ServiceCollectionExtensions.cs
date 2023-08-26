eeac48f396b9eea2076f318acf8e48f1efd1f7be
Yangtao Jiang <jiangyangtao1@qq.com>
2023-08-26 18:56:28 +0800

    Add Repository.MySql

---
+++ src/Repository/Yangtao.Hosting.Repository.MySql/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Yangtao.Hosting.Repository.Core;
+
+namespace Yangtao.Hosting.Repository.MySql
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services, string connectionString)
+        {
+            services.AddRepository(options =>
+            {
+                var serverVersion = ServerVersion.AutoDetect(connectionString);
+                options.UseLazyLoadingProxies().UseMySql(connectionString, serverVersion);
+            });
+
+            return services;
+        }
+    }
+}
