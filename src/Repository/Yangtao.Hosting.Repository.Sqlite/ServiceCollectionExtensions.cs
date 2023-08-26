ac188b8bde010bea5d003b28b2ffd5dde497491a
Yangtao Jiang <jiangyangtao1@qq.com>
2023-08-26 19:30:39 +0800

    Add Repository.Sqlite

---
+++ src/Repository/Yangtao.Hosting.Repository.Sqlite/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Yangtao.Hosting.Repository.Core;
+
+namespace Yangtao.Hosting.Repository.Sqlite
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRepository(this IServiceCollection services, string connectionString)
+        {
+            services.AddRepository(options =>
+            {
+                options.UseLazyLoadingProxies().UseSqlite(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
