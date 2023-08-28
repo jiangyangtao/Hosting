027c16955e4b910bd1d2fcea81ab3adb49daf471
Yangtao Jiang <jiangyangtao1@qq.com>
2023-08-28 19:37:26 +0800

    Add ApiVersion

---
+++ src/Yangtao.Hosting.Endpoint/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Yangtao.Hosting.Endpoint
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApiVersion(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.UseApiBehavior = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+            services.AddVersionedApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'V";
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+            return services;
+        }
+    }
+}
