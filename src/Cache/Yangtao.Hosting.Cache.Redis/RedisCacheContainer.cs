441209e9d71fd424b78d11e12563309142c67b20
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-01 08:25:54 +0800

    Adjustment nuget package

--- src/Cache/Yangtao.Hosting.Cache.Redis/RedisCacheContainer.cs
+++ src/Cache/Yangtao.Hosting.Cache.Redis/RedisCacheContainer.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using StackExchange.Redis;
-using Yangtao.Hosting.Cache.Abstractions;
 using Yangtao.Hosting.Cache.Redis.Abstracts;
 
 namespace Yangtao.Hosting.Cache.Redis
