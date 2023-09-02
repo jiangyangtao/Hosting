11a5b215d6a4627910560d0f642b6cbfad47b284
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-03 03:37:38 +0800

    Add QueryableExtensions

---
+++ src/Repository/Yangtao.Hosting.Repository.Core/QueryableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Yangtao.Hosting.Repository.Abstractions;
+
+namespace Yangtao.Hosting.Repository.Core
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<TModel> Pagination<TModel>(this IQueryable<TModel> queryable, PaginationBase paginationParam) where TModel : IModel
+        {
+            return queryable.Skip(paginationParam.Start).Take(paginationParam.Size);
+        }
+
+        public static IQueryable<TEntity> OrderByDescCreateTime<TEntity>(this IQueryable<TEntity> queryable) where TEntity : IEntity
+        {
+            return queryable.OrderByDescending(a => a.CreateTime);
+        }
+
+        public static IQueryable<TEntity> DefaultOrderPagination<TEntity>(this IQueryable<TEntity> queryable, PaginationBase paginationParam) where TEntity : IEntity
+        {
+            return queryable.OrderByDescCreateTime().Pagination(paginationParam);
+        }
+    }
+}
