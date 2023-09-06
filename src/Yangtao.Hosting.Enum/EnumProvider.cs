23015d21f672bf12dcad7f586b0521c04d2867bc
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-07 01:52:38 +0800

    Create EnumProvider

---
+++ src/Yangtao.Hosting.Enum/EnumProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yangtao.Hosting.Enum
+{
+    internal class EnumProvider<TEnum> : EnumHandlerBase, IEnumProvider<TEnum> where TEnum : struct, System.Enum
+    {
+        public EnumProvider() : base(typeof(TEnum))
+        {
+        }
+    }
+}
---
+++ src/Yangtao.Hosting.Enum/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yangtao.Hosting.Enum
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEnum(this IServiceCollection services)
+        {
+           
+
+            return services;
+        }
+    }
+}
