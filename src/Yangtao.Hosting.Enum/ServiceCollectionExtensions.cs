10eeebc54229549d0b85873feff85dad980fef10
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-08 00:20:10 +0800

    FixBugs git push

--- src/Yangtao.Hosting.Enum/ServiceCollectionExtensions.cs
+++ src/Yangtao.Hosting.Enum/ServiceCollectionExtensions.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyModel;
+using System.Reflection;
+using Yangtao.Hosting.Extensions;
 
 namespace Yangtao.Hosting.Enum
 {
@@ -11,9 +9,29 @@ namespace Yangtao.Hosting.Enum
     {
         public static IServiceCollection AddEnum(this IServiceCollection services)
         {
-           
+            var assemblys = DependencyContext.Default.CompileLibraries.Where(a => a.Type == "project").Select(a => Assembly.Load(a.Name)).ToArray();
+            if (assemblys.IsNullOrEmpty()) return services;
+
+            var registerMethod = typeof(ServiceCollectionExtensions).GetMethod(nameof(RegisterEnumProvider), BindingFlags.Static | BindingFlags.NonPublic);
+            foreach (var assembly in assemblys)
+            {
+                var enumTypes = assembly.GetTypes().Where(a => a.IsEnum).ToArray();
+                if (enumTypes.IsNullOrEmpty()) continue;
+
+                foreach (var enumType in enumTypes)
+                {
+                    var method = registerMethod.MakeGenericMethod(enumType);
+                    registerMethod.GetGenericMethodDefinition();
+                    registerMethod.Invoke(null, new object[] { services });
+                }
+            }
 
             return services;
+        }
+
+        private static void RegisterEnumProvider<TEnum>(IServiceCollection services) where TEnum : struct, System.Enum
+        {
+            services.AddSingleton<EnumProvider<TEnum>>();
         }
     }
 }
