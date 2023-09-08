e570a45418054fdc30e994fd3f58da6d1216ca53
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-09 02:39:16 +0800

    Update HttpResultController

--- src/Yangtao.Hosting.Controller/HttpResultController.cs
+++ src/Yangtao.Hosting.Controller/HttpResultController.cs
@@ -1,17 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Yangtao.Hosting.Core.HttpErrorResult;
 using Yangtao.Hosting.Mvc.HttpResponseResult.HttpResult;
 
 namespace Yangtao.Hosting.Controller
 {
     public abstract class HttpResultController : ControllerBase
     {
+        #region BadRequest  
+
+        [NonAction]
+        public HttpBadRequestResult ResponseBadRequest() => new();
+
         [NonAction]
         public HttpBadRequestResult ResponseBadRequest(string message) => new(message);
 
         [NonAction]
         public HttpBadRequestResult ResponseBadRequest(int code, string message) => new(code, message);
 
+        [NonAction]
+        public TResult ResponseBadRequest<TResult>() => HttpErrorResult.ResponseBadRequest<TResult>();
 
+        [NonAction]
+        public TResult ResponseBadRequest<TResult>(string message) => HttpErrorResult.ResponseBadRequest<TResult>(message);
+
+        [NonAction]
+        public TResult ResponseBadRequest<TResult>(int code, string message) => HttpErrorResult.ResponseBadRequest<TResult>(code, message);
+
+        #endregion
+
+        #region Conflict
 
         [NonAction]
         public HttpConflictResult ResponseConflict() => new();
@@ -22,7 +39,18 @@ namespace Yangtao.Hosting.Controller
         [NonAction]
         public HttpConflictResult ResponseConflict(int code, string message) => new(code, message);
 
+        [NonAction]
+        public TResult ResponseConflict<TResult>() => HttpErrorResult.ResponseConflict<TResult>();
 
+        [NonAction]
+        public TResult ResponseConflict<TResult>(string message) => HttpErrorResult.ResponseConflict<TResult>(message);
+
+        [NonAction]
+        public TResult ResponseConflict<TResult>(int code, string message) => HttpErrorResult.ResponseConflict<TResult>(code, message);
+
+        #endregion
+
+        #region Forbid      
 
         [NonAction]
         public HttpForbidResult ResponseForbid() => new();
@@ -33,7 +61,18 @@ namespace Yangtao.Hosting.Controller
         [NonAction]
         public HttpForbidResult ResponseForbid(int code, string message) => new(code, message);
 
+        [NonAction]
+        public TResult ResponseForbid<TResult>() => HttpErrorResult.ResponseForbid<TResult>();
 
+        [NonAction]
+        public TResult ResponseForbid<TResult>(string message) => HttpErrorResult.ResponseForbid<TResult>(message);
+
+        [NonAction]
+        public TResult ResponseForbid<TResult>(int code, string message) => HttpErrorResult.ResponseForbid<TResult>(code, message);
+
+        #endregion
+
+        #region NotFound
 
 
         [NonAction]
@@ -45,9 +84,18 @@ namespace Yangtao.Hosting.Controller
         [NonAction]
         public HttpNotFoundResult ResponseNotFound(int code, string message) => new(code, message);
 
+        [NonAction]
+        public TResult ResponseNotFound<TResult>() => HttpErrorResult.ResponseNotFound<TResult>();
 
+        [NonAction]
+        public TResult ResponseNotFound<TResult>(string message) => HttpErrorResult.ResponseNotFound<TResult>(message);
 
+        [NonAction]
+        public TResult ResponseNotFound<TResult>(int code, string message) => HttpErrorResult.ResponseNotFound<TResult>(code, message);
 
+        #endregion
+
+        #region ServerError    
 
         [NonAction]
         public HttpServerErrorResult ResponseServerError(string message) => new(message);
@@ -55,9 +103,15 @@ namespace Yangtao.Hosting.Controller
         [NonAction]
         public HttpServerErrorResult ResponseServerError(int code, string message) => new(code, message);
 
+        [NonAction]
+        public TResult ResponseServerError<TResult>(string message) => HttpErrorResult.ResponseServerError<TResult>(message);
 
+        [NonAction]
+        public TResult ResponseServerError<TResult>(int code, string message) => HttpErrorResult.ResponseServerError<TResult>(code, message);
 
+        #endregion
 
+        #region Unauthorized 
 
         [NonAction]
         public HttpUnauthorizedResult ResponseUnauthorized() => new();
@@ -68,10 +122,18 @@ namespace Yangtao.Hosting.Controller
         [NonAction]
         public HttpUnauthorizedResult ResponseUnauthorized(int code, string message) => new(code, message);
 
+        [NonAction]
+        public TResult ResponseUnauthorized<TResult>() => HttpErrorResult.ResponseUnauthorized<TResult>();
 
+        [NonAction]
+        public TResult ResponseUnauthorized<TResult>(string message) => HttpErrorResult.ResponseUnauthorized<TResult>(message);
 
+        [NonAction]
+        public TResult ResponseUnauthorized<TResult>(int code, string message) => HttpErrorResult.ResponseUnauthorized<TResult>(code, message);
 
+        #endregion
 
+        #region Success    
 
         [NonAction]
         public HttpSuccessResult ResponseSuccess() => new();
@@ -87,6 +149,8 @@ namespace Yangtao.Hosting.Controller
 
         [NonAction]
         public HttpSuccessResult<TResult> ResponseSuccess<TResult>(IEnumerable<TResult> results, int count = 0) => new(results, count);
+
+        #endregion
 
     }
 }
