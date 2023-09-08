5141682a29066fa58bb6cb4548116c11fe8c2c25
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-09 01:40:43 +0800

    Update HttpFailResult

--- src/Yangtao.Hosting.Core/HttpErrorResult/HttpErrorResult.cs
+++ src/Yangtao.Hosting.Core/HttpErrorResult/HttpErrorResult.cs
@@ -36,6 +36,7 @@ namespace Yangtao.Hosting.Core.HttpErrorResult
             return JsonConvert.SerializeObject(ErrorResult);
         }
 
+        #region BadRequest
 
 
         public static void ResponseBadRequest() => throw new HttpErrorResult(400);
@@ -44,7 +45,16 @@ namespace Yangtao.Hosting.Core.HttpErrorResult
 
         public static void ResponseBadRequest(int code, string message) => throw new HttpErrorResult(400, code, message);
 
+        public static TResult ResponseBadRequest<TResult>() => throw new HttpErrorResult(400);
 
+        public static TResult ResponseBadRequest<TResult>(string message) => throw new HttpErrorResult(400, message);
+
+        public static TResult ResponseBadRequest<TResult>(int code, string message) => throw new HttpErrorResult(400, code, message);
+
+
+        #endregion
+
+        #region Conflict     
 
         public static void ResponseConflict() => throw new HttpErrorResult(409);
 
@@ -52,6 +62,15 @@ namespace Yangtao.Hosting.Core.HttpErrorResult
 
         public static void ResponseConflict(int code, string message) => throw new HttpErrorResult(409, code, message);
 
+        public static TResult ResponseConflict<TResult>() => throw new HttpErrorResult(409);
+
+        public static TResult ResponseConflict<TResult>(string message) => throw new HttpErrorResult(409, message);
+
+        public static TResult ResponseConflict<TResult>(int code, string message) => throw new HttpErrorResult(409, code, message);
+
+        #endregion
+
+        #region Forbid
 
 
         public static void ResponseForbid() => throw new HttpErrorResult(403);
@@ -60,7 +79,15 @@ namespace Yangtao.Hosting.Core.HttpErrorResult
 
         public static void ResponseForbid(int code, string message) => throw new HttpErrorResult(403, code, message);
 
+        public static TResult ResponseForbid<TResult>() => throw new HttpErrorResult(403);
 
+        public static TResult ResponseForbid<TResult>(string message) => throw new HttpErrorResult(403, message);
+
+        public static TResult ResponseForbid<TResult>(int code, string message) => throw new HttpErrorResult(403, code, message);
+
+        #endregion
+
+        #region NotFound
 
         public static void ResponseNotFound() => throw new HttpErrorResult(404);
 
@@ -68,6 +95,15 @@ namespace Yangtao.Hosting.Core.HttpErrorResult
 
         public static void ResponseNotFound(int code, string message) => throw new HttpErrorResult(404, code, message);
 
+        public static TResult ResponseNotFound<TResult>() => throw new HttpErrorResult(404);
+
+        public static TResult ResponseNotFound<TResult>(string message) => throw new HttpErrorResult(404, message);
+
+        public static TResult ResponseNotFound<TResult>(int code, string message) => throw new HttpErrorResult(404, code, message);
+
+        #endregion
+
+        #region Unauthorized
 
         public static void ResponseUnauthorized() => throw new HttpErrorResult(401);
 
@@ -75,11 +111,28 @@ namespace Yangtao.Hosting.Core.HttpErrorResult
 
         public static void ResponseUnauthorized(int code, string message) => throw new HttpErrorResult(401, code, message);
 
+        public static TResult ResponseUnauthorized<TResult>() => throw new HttpErrorResult(401);
+
+        public static TResult ResponseUnauthorized<TResult>(string message) => throw new HttpErrorResult(401, message);
+
+        public static TResult ResponseUnauthorized<TResult>(int code, string message) => throw new HttpErrorResult(401, code, message);
+
+        #endregion
+
+        #region ServerError   
 
         public static void ResponseServerError() => throw new HttpErrorResult(500);
 
         public static void ResponseServerError(string message) => throw new HttpErrorResult(500, message);
 
         public static void ResponseServerError(int code, string message) => throw new HttpErrorResult(500, code, message);
+
+        public static TResult ResponseServerError<TResult>() => throw new HttpErrorResult(500);
+
+        public static TResult ResponseServerError<TResult>(string message) => throw new HttpErrorResult(500, message);
+
+        public static TResult ResponseServerError<TResult>(int code, string message) => throw new HttpErrorResult(500, code, message);
+
+        #endregion
     }
 }
