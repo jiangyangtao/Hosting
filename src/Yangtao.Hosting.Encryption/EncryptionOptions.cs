a08a54894d26c6e9d6f91d43720df1e1c8339ca4
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-09 21:23:34 +0800

    Add DI

---
+++ src/Yangtao.Hosting.Encryption/EncryptionOptions.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Yangtao.Hosting.Encryption
+{
+    public class EncryptionOptions
+    {
+        public string SecretKey { get; }
+
+        public string Iv { get; }
+    }
+}
---
+++ src/Yangtao.Hosting.Encryption/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Yangtao.Hosting.Encryption.AdvancedEncryptionStandard;
+using Yangtao.Hosting.Encryption.TripleDataEncryptionAlgorithm;
+
+namespace Yangtao.Hosting.Encryption
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAesEncryption(this IServiceCollection services, Action<EncryptionOptions> actionOptions)
+        {
+            var options = new EncryptionOptions();
+            actionOptions(options);
+
+            services.AddSingleton(new AesProvider(options.SecretKey, options.Iv));
+            return services;
+        }
+
+        public static IServiceCollection AddTripleDesEncryption(this IServiceCollection services, Action<EncryptionOptions> actionOptions)
+        {
+            var options = new EncryptionOptions();
+            actionOptions(options);
+
+            services.AddSingleton(new TripleDesProvider(options.SecretKey, options.Iv));
+            return services;
+        }
+    }
+}
