4f1e10a307b1e943a4df018ce525e01221e2843a
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-10 01:22:30 +0800

    Add RsaSecretKey

---
+++ src/Yangtao.Hosting.Encryption/RsaAlgorithm/RsaSecretKey.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Yangtao.Hosting.Encryption.RsaAlgorithm
+{
+    public class RsaSecretKey
+    {
+        internal RsaSecretKey() { }
+
+        public string PublicKey { get; internal set; }
+
+        public string PrivateKey { get; internal set; }
+    }
+}
---
+++ src/Yangtao.Hosting.Encryption/RsaAlgorithm/RsaSecretKeyCreator.cs
@@ -0,0 +1,25 @@
+﻿using RSAExtensions;
+using System.Security.Cryptography;
+
+namespace Yangtao.Hosting.Encryption.RsaAlgorithm
+{
+    public class RsaSecretKeyCreator
+    {
+        internal RsaSecretKeyCreator()
+        {
+        }
+
+        public static RsaSecretKey Generate(RSAKeyType keyType = RSAKeyType.Pkcs8)
+        {
+            using var rsa = RSA.Create();
+
+            var privateKey = rsa.ExportPrivateKey(keyType);
+            var publicKey = rsa.ExportPublicKey(keyType);
+            return new RsaSecretKey
+            {
+                PrivateKey = privateKey,
+                PublicKey = publicKey,
+            };
+        }
+    }
+}
