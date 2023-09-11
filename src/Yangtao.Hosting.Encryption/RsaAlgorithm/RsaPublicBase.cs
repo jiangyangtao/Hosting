b1fae26beae8bd66ef4503740029e9a4722f9331
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-12 00:29:54 +0800

    Add VerifyData

--- src/Yangtao.Hosting.Encryption/RsaAlgorithm/RsaPublicBase.cs
+++ src/Yangtao.Hosting.Encryption/RsaAlgorithm/RsaPublicBase.cs
@@ -36,5 +36,21 @@ namespace Yangtao.Hosting.Encryption.RsaAlgorithm
         public string EncryptBigData(string plaintext) => EncryptBigData(plaintext, RSAEncryptionPadding.Pkcs1);
 
         public string EncryptBigData(string plaintext, RSAEncryptionPadding padding) => Rsa.EncryptBigData(plaintext, padding);
+
+        public bool VerifyData(string value, string singData) => VerifyData(value, singData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+        public bool VerifyData(string value, string singData, HashAlgorithmName algorithmName) => VerifyData(value, singData, algorithmName, RSASignaturePadding.Pkcs1);
+
+        public bool VerifyData(string value, string singData, RSASignaturePadding signaturePadding) => VerifyData(value, singData, HashAlgorithmName.SHA256, signaturePadding);
+
+        public bool VerifyData(string value, string singData, HashAlgorithmName algorithmName, RSASignaturePadding signaturePadding)
+        {
+            if (value.IsNullOrEmpty()) throw new ArgumentNullException(nameof(value));
+            if (singData.IsNullOrEmpty()) throw new ArgumentNullException(nameof(singData));
+
+            var valueBytes = Encoding.UTF8.GetBytes(value);
+            var singDataBytes = Encoding.UTF8.GetBytes(singData);
+            return Rsa.VerifyData(valueBytes, singDataBytes, algorithmName, signaturePadding);
+        }
     }
 }
