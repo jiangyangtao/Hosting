2f18854441194823345ace7930ac07d16cd062ab
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-29 11:55:23 +0800

    Finished ServerConfigurationProvider

--- src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/Abstractions/IServerConfigurationProvider.cs
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/Abstractions/IServerConfigurationProvider.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Yangtao.Hosting.SignatureValidation.Core.Configurations;
+using Yangtao.Hosting.SignatureValidation.Server.Configurations;
 
 namespace Yangtao.Hosting.SignatureValidation.Server.Abstractions
 {
     internal interface IServerConfigurationProvider
     {
+        ServerValidationOptions ServerValidationOptions { get; }
+
+        HmacShaOptions? HmacShaOptions { get; }
+
+        RsaSignatureOptions? RsaSignatureOptions { get; }
+
+        RsaEncryptionOptions? RsaEncryptionOptions { get; }
     }
 }
--- src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/ServerConfigurationProvider.cs
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/ServerConfigurationProvider.cs
@@ -1,16 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Options;
+using Yangtao.Hosting.SignatureValidation.Core.Configurations;
 using Yangtao.Hosting.SignatureValidation.Server.Abstractions;
+using Yangtao.Hosting.SignatureValidation.Server.Configurations;
 
 namespace Yangtao.Hosting.SignatureValidation.Server
 {
     internal class ServerConfigurationProvider : IServerConfigurationProvider
     {
-        public ServerConfigurationProvider()
+        private readonly IOptions<ServerValidationOptions> _serverValidationOptions;
+        private readonly IOptions<HmacShaOptions> _hmacShaOptions;
+        private readonly IOptions<RsaSignatureOptions> _rsaSignatureOptions;
+        private readonly IOptions<RsaEncryptionOptions> _rsaEncryptionOptions;
+
+        public ServerConfigurationProvider(IOptions<ServerValidationOptions> serverValidationOptions,
+            IOptions<HmacShaOptions> hmacShaOptions,
+            IOptions<RsaSignatureOptions> rsaSignatureOptions,
+            IOptions<RsaEncryptionOptions> rsaEncryptionOptions)
         {
+            _serverValidationOptions = serverValidationOptions;
+            _hmacShaOptions = hmacShaOptions;
+            _rsaSignatureOptions = rsaSignatureOptions;
+            _rsaEncryptionOptions = rsaEncryptionOptions;
         }
+
+        public ServerValidationOptions ServerValidationOptions => _serverValidationOptions.Value;
+
+        public HmacShaOptions? HmacShaOptions => _hmacShaOptions.Value ?? null;
+
+        public RsaSignatureOptions? RsaSignatureOptions => _rsaSignatureOptions.Value ?? null;
+
+        public RsaEncryptionOptions? RsaEncryptionOptions => _rsaEncryptionOptions.Value ?? null;
     }
 }
