118040cf0422eae4f554b17029e06296c243f129
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-01 14:58:59 +0800

    Add DapperOptions

--- src/Repository/Yangtao.Hosting.Repository.Dapper/DapperServiceCollectionExtensions.cs
+++ src/Repository/Yangtao.Hosting.Repository.Dapper/DapperServiceCollectionExtensions.cs
@@ -1,7 +1,18 @@
-﻿namespace Yangtao.Hosting.Repository.Dapper
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Yangtao.Hosting.Repository.Dapper
 {
     public static class DapperServiceCollectionExtensions
     {
-
+        public static IServiceCollection AddDapperRepository(this IServiceCollection services, Action<DapperOptions> optionAction)
+        {
+            var options = new DapperOptions();
+            optionAction(options);
+            services.Configure<DapperOptions>(a =>
+            {
+                a.DbConnectionString = options.DbConnectionString;
+            });
+            return services;
+        }
     }
 }
