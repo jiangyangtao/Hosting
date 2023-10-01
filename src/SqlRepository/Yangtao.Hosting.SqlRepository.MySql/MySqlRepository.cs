2b8ffb76fcabdc101b379af6cfe03f45b4a87424
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-02 03:53:56 +0800

    Add Repository

---
+++ src/SqlRepository/Yangtao.Hosting.SqlRepository.MySql/MySqlRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using Yangtao.Hosting.SqlRepository.Core;
+
+namespace Yangtao.Hosting.SqlRepository.MySql
+{
+    internal class MySqlRepository : SqlRepositoryBase
+    {
+        public MySqlRepository()
+        {
+        }
+
+        protected override DbConnection GetDbConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SqlRepository/Yangtao.Hosting.SqlRepository.SqlServer/SqlServerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Common;
+using Yangtao.Hosting.SqlRepository.Core;
+
+namespace Yangtao.Hosting.SqlRepository.SqlServer
+{
+    internal class SqlServerRepository : SqlRepositoryBase
+    {
+        public SqlServerRepository()
+        {
+        }
+
+        protected override DbConnection GetDbConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
