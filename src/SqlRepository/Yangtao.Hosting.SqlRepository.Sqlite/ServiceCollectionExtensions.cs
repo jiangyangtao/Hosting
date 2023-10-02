46611bba6f1b6caca6d90f3d56145f9d873a116a
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-02 16:11:12 +0800

    Finished SqlRepository of Sqlite

--- src/SqlRepository/Yangtao.Hosting.SqlRepository.Sqlite/ServiceCollectionExtensions.cs
+++ src/SqlRepository/Yangtao.Hosting.SqlRepository.Sqlite/ServiceCollectionExtensions.cs
@@ -1,7 +1,44 @@
-﻿namespace Yangtao.Hosting.SqlRepository.Sqlite
-{
-    public class ServiceCollectionExtensions
-    {
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Yangtao.Hosting.SqlRepository.Abstractions;
+using Yangtao.Hosting.SqlRepository.Core;
+using Yangtao.Hosting.Extensions;
 
+namespace Yangtao.Hosting.SqlRepository.Sqlite
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSqlRepository(this IServiceCollection services)
+        {
+            var connectionString = services.GetConnectionString();
+
+            return services.AddSqlRepository(connectionString);
+        }
+
+        public static IServiceCollection AddSqlRepository(this IServiceCollection services, string connectionString)
+        {
+            return services.AddSqlRepository(options => options.ConnectionsString = connectionString);
+        }
+
+        public static IServiceCollection AddSqlRepository(this IServiceCollection services, Action<SqlRepositoryOptions> optionAction)
+        {
+            services.AddSingleton<ISqlRepository, SqliteRepository>();
+            return services.AddRepositoryCore(optionAction);
+        }
+
+        private static string GetConnectionString(this IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+            var connectionStringsConfig = configuration.GetSection("ConnectionStrings") ?? throw new KeyNotFoundException("In configuration not found ConnectionStrings");
+
+            var mysqlConnectionStringConfig = connectionStringsConfig.GetSection("Sqlite");
+            mysqlConnectionStringConfig ??= connectionStringsConfig.GetSection("sqlite");
+
+            if (mysqlConnectionStringConfig == null) throw new KeyNotFoundException("In configuration not found Sqlite or sqlite of ConnectionStrings");
+            if (mysqlConnectionStringConfig.Value.IsNullOrEmpty()) throw new KeyNotFoundException("The ConnectionString can not be null or empty.");
+
+            return mysqlConnectionStringConfig.Value;
+        }
     }
 }
--- src/SqlRepository/Yangtao.Hosting.SqlRepository.Sqlite/SqliteRepository.cs
+++ src/SqlRepository/Yangtao.Hosting.SqlRepository.Sqlite/SqliteRepository.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Options;
+using System.Data.Common;
+using Yangtao.Hosting.SqlRepository.Core;
 
 namespace Yangtao.Hosting.SqlRepository.Sqlite
 {
-    internal class SqliteRepository
+    internal class SqliteRepository : SqlRepositoryBase
     {
+        private readonly SqlRepositoryOptions RepositoryOptions;
+
+        public SqliteRepository(IOptions<SqlRepositoryOptions> options)
+        {
+            RepositoryOptions = options.Value;
+        }
+
+        protected override DbConnection GetDbConnection()
+        {
+            var sqliteConnection = new SqliteConnection(RepositoryOptions.ConnectionsString);
+            sqliteConnection.Open();
+
+            return sqliteConnection;
+        }
+
+        protected override async Task<DbConnection> GetDbConnectionAsync()
+        {
+            var sqliteConnection = new SqliteConnection(RepositoryOptions.ConnectionsString);
+            await sqliteConnection.OpenAsync();
+
+            return sqliteConnection;
+        }
     }
 }
