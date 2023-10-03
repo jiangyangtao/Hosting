6be9f882c0deb8cfc3c55049a5ffdee69ee0b07d
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-04 02:24:56 +0800

    Add Configurationa and enums

---
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Core/Configurations/RsaConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Yangtao.Hosting.SignatureValidation.Core.Configurations
+{
+    public abstract class RsaConfigurationBase
+    {
+        public RsaAlgorithmType AlgorithmType { set; get; }
+
+        public RSAKeyType RSAKeyType { set; get; }
+
+        public string Algorithm
+        {
+            get
+            {
+                if (AlgorithmType == RsaAlgorithmType.RsaSha256) return SecurityAlgorithms.RsaSha256;
+                if (AlgorithmType == RsaAlgorithmType.RsaSha384) return SecurityAlgorithms.RsaSha384;
+
+                return SecurityAlgorithms.RsaSha512;
+            }
+        }
+    }
+}
