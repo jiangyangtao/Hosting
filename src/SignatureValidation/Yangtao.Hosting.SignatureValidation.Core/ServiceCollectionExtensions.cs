16b78b1234fb07bdf959a10a564c650646729cce
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-04 04:16:43 +0800

    Update SignatureValidation

---
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Yangtao.Hosting.SignatureValidation.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSignatureValidationCore(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
