7dc123d1b4a1e6d0855c2e0da9d29753436d5a3d
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-05 04:21:35 +0800

    Finished HmacShaPorviderFactory

--- src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Core/Abstractions/IHmacShaProvider.cs
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Core/Abstractions/IHmacShaProvider.cs
@@ -1,8 +1,11 @@
-﻿
+﻿using Yangtao.Hosting.SignatureValidation.Core.Enums;
+
 namespace Yangtao.Hosting.SignatureValidation.Core.Abstractions
 {
     internal interface IHmacShaProvider
     {
+        HmacShaAlgorithmType HmacShaAlgorithmType { get; }
+
         /// <summary>
         /// 签名
         /// </summary>
--- src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Core/HmacShaPorviders/HmacShaPorviderFactory.cs
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Core/HmacShaPorviders/HmacShaPorviderFactory.cs
@@ -1,16 +1,27 @@
-﻿using Yangtao.Hosting.SignatureValidation.Core.Abstractions;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Yangtao.Hosting.SignatureValidation.Core.Abstractions;
 
 namespace Yangtao.Hosting.SignatureValidation.Core.HmacShaPorviders
 {
     internal class HmacShaPorviderFactory : IHmacShaPorviderFactory
     {
-        public HmacShaPorviderFactory()
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ISignatureValidationConfigurationProvider _configurationProvider;
+
+        public HmacShaPorviderFactory(
+            IServiceProvider serviceProvider,
+            ISignatureValidationConfigurationProvider configurationProvider)
         {
+            _configurationProvider = configurationProvider;
+            _serviceProvider = serviceProvider;
         }
 
         public IHmacShaProvider CreateIHmacShaProvider()
         {
-            throw new NotImplementedException();
+            var hmacShaConfiguration = _configurationProvider.GetHmacShaConfiguration();
+            var provider = _serviceProvider.GetServices<IHmacShaProvider>().FirstOrDefault(a => a.HmacShaAlgorithmType == hmacShaConfiguration.HmacShaAlgorithmType);
+
+            return provider ?? throw new KeyNotFoundException($"Not found {hmacShaConfiguration.HmacShaAlgorithmType} the HmacShaProvider.");
         }
     }
 }
