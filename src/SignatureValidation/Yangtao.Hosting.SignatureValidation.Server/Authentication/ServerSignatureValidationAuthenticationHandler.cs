b43ac1c8537ec4ac38f8ccaeb9537a399abac534
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-06 04:04:54 +0800

    Move Test the projects

--- src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/Authentication/ServerSignatureValidationAuthenticationHandler.cs
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/Authentication/ServerSignatureValidationAuthenticationHandler.cs
@@ -2,22 +2,30 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
+using System.Security.Claims;
 using System.Text.Encodings.Web;
 using Yangtao.Hosting.SignatureValidation.Core.Configurations;
+using Yangtao.Hosting.SignatureValidation.Server.Configurations;
 
 namespace Yangtao.Hosting.SignatureValidation.Server.Authentication
 {
     internal class ServerSignatureValidationAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
     {
-        public ServerSignatureValidationAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) 
+        private readonly AuthenticateResult EmptyAuthenticateSuccessResult;
+
+        public ServerSignatureValidationAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
             : base(options, logger, encoder, clock)
         {
+            var ticket = new AuthenticationTicket(new ClaimsPrincipal(), ServerSignatureValidationDefaultKeys.ServerSignatureValidationScheme);
+            EmptyAuthenticateSuccessResult = AuthenticateResult.Success(ticket);
         }
 
         protected override Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            Context.Request.Headers.TryGetValue(SignatureValidationDefaultKeys.SignatureKey, out StringValues value);
-            throw new NotImplementedException();
+            var hasSignatureKey = Context.Request.Headers.TryGetValue(SignatureValidationDefaultKeys.SignatureKey, out StringValues value);
+            if (hasSignatureKey == false) AuthenticateResult.NoResult();
+
+            return Task.FromResult(EmptyAuthenticateSuccessResult);
         }
     }
 }
