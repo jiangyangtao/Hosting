2a5bf399a71a0d2bc18e5e2e5b6a4e7a6cc0519f
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-05 22:16:15 +0800

    Add AuthenticationHandler and GrpcInterceptor

---
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/Authentication/ServerSignatureValidationAuthenticationHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Text.Encodings.Web;
+
+namespace Yangtao.Hosting.SignatureValidation.Server.Authentication
+{
+    internal class ServerSignatureValidationAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public ServerSignatureValidationAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) 
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Server/GrpcInterceptor/ServerSignatureValidationInterceptor.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core.Interceptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yangtao.Hosting.SignatureValidation.Server.GrpcInterceptor
+{
+    internal class ServerSignatureValidationInterceptor : Interceptor
+    {
+    }
+}
