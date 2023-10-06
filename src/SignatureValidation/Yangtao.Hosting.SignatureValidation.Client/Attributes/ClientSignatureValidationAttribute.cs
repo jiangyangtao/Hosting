a8f8af3f4f0783bb462ff72fa903a8cdf3996758
Yangtao Jiang <jiangyangtao1@qq.com>
2023-10-07 07:14:29 +0800

    Add ClientSignatureValidationAttribute

---
+++ src/SignatureValidation/Yangtao.Hosting.SignatureValidation.Client/Attributes/ClientSignatureValidationAttribute.cs
@@ -0,0 +1,5 @@
+﻿namespace Yangtao.Hosting.SignatureValidation.Client.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ClientSignatureValidationAttribute : Attribute { }
+}
