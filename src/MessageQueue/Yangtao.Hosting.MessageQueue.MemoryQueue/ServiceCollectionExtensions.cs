495f01a21778359c2ca8eb4b7f3d3f5309b0905a
Yangtao Jiang <jiangyangtao1@qq.com>
2023-11-15 02:09:43 +0800

    Add Yangtao.Hosting.MessageQueue.MemoryQueue the project

---
+++ src/MessageQueue/Yangtao.Hosting.MessageQueue.MemoryQueue/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Yangtao.Hosting.MessageQueue.MemoryQueue
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMemoryQueue(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
