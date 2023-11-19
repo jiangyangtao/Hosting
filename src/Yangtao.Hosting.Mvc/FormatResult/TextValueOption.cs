fd133d304e69584cd5d86a51bc8c0ef1c026fe5f
Yangtao Jiang <jiangyangtao1@qq.com>
2023-11-20 00:54:42 +0800

    Add TextValueOption

---
+++ src/Yangtao.Hosting.Mvc/FormatResult/TextValueOption.cs
@@ -0,0 +1,18 @@
+﻿namespace Yangtao.Hosting.Mvc.FormatResult
+{
+    public class TextValueOption
+    {
+        public TextValueOption(string value, string text, string description = "")
+        {
+            Value = value;
+            Text = text;
+            Description = description;
+        }
+
+        public string Value { set; get; }
+
+        public string Text { set; get; }
+
+        public string Description { set; get; }
+    }
+}
