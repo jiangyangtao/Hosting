9b8c21201cc148c60bd9e4e337bf4e86e51d8880
Yangtao Jiang <jiangyangtao1@qq.com>
2024-04-11 03:27:40 +0800

    Add SignAuthenticationGrpcServerInterceptor

---
+++ src/Yangtao.Hosting.GrpcServer/Options/GrpcServerOptions.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.AspNetCore.Server;
+using Yangtao.Hosting.GrpcCore.Options;
+
+namespace Yangtao.Hosting.GrpcServer.Options
+{
+    public class GrpcServerOptions
+    {
+        internal GrpcServerOptions()
+        {
+        }
+
+        public GrpcServiceOptions GrpcServiceOptions { set; get; }
+
+        public SignAuthenticationType? SignAuthenticationType { set; get; }
+
+        public AesSignOptions? AesSignOptions { set; get; }
+
+        public RsaPrivateSignOptions? RsaPrivateSignOptions { set; get; }
+    }
+}
--- src/Yangtao.Hosting.GrpcServer/ServiceCollectionExtensions.cs
+++ src/Yangtao.Hosting.GrpcServer/ServiceCollectionExtensions.cs
@@ -1,18 +1,42 @@
-﻿using Grpc.AspNetCore.Server;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Yangtao.Hosting.GrpcCore.Options;
+using Yangtao.Hosting.GrpcCore;
 using Yangtao.Hosting.GrpcServer.Interceptors;
+using Yangtao.Hosting.GrpcServer.Options;
 
 namespace Yangtao.Hosting.GrpcServer
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddGrpcServer(this IServiceCollection services, Action<GrpcServiceOptions> action)
+        public static IServiceCollection AddGrpcServer(this IServiceCollection services, Action<GrpcServerOptions> action)
         {
+            var serverOptions = new GrpcServerOptions();
             services.AddGrpc(options =>
+             {
+                 serverOptions.GrpcServiceOptions = options;
+                 options.Interceptors.Add<GrpcHttpErrorHandleInterceptor>();
+                 action(serverOptions);
+
+                 if (serverOptions.SignAuthenticationType.HasValue)
+                     options.Interceptors.Add<SignAuthenticationGrpcServerInterceptor>();
+             });
+
+
+            if (serverOptions.SignAuthenticationType.HasValue)
             {
-                options.Interceptors.Add<GrpcHttpErrorHandleInterceptor>();
-                action(options);
-            });
+                if (serverOptions.SignAuthenticationType == SignAuthenticationType.Aes && serverOptions.AesSignOptions == null)
+                    throw new ArgumentException(nameof(serverOptions.AesSignOptions));
+
+                if (serverOptions.SignAuthenticationType == SignAuthenticationType.RSA && serverOptions.RsaPrivateSignOptions == null)
+                    throw new ArgumentException(nameof(serverOptions.RsaPrivateSignOptions));
+
+                services.AddGrpcCore(options =>
+                {
+                    options.SignAuthenticationType = serverOptions.SignAuthenticationType;
+                    options.AesSignOptions = serverOptions.AesSignOptions;
+                    options.RsaPrivateSignOptions = serverOptions.RsaPrivateSignOptions;
+                });
+            }
 
             return services;
         }
