864711470fdf4adfebf0b7198bf8401450b265c7
Yangtao Jiang <jiangyangtao1@qq.com>
2024-04-11 09:44:35 +0800

    Add DataPaginationResult

---
+++ src/Yangtao.Hosting.Mvc/FormatResult/DataPaginationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Yangtao.Hosting.Mvc.FormatResult
+{
+    public class DataPaginationResult<TResult> : PaginationResult<TResult> where TResult : class
+    {
+        public DataPaginationResult(long count, Func<TResult[]> func) : base(count)
+        {
+            List = func();
+        }
+    }
+}
