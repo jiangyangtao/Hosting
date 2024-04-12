c75354003e7288a4609589af6ccb338231f88100
Yangtao Jiang <jiangyangtao1@qq.com>
2024-04-12 18:03:49 +0800

    Add HttpContextAccessorExtensions

---
+++ src/Extensions/Yangtao.Hosting.Http.Extensions/HttpContextAccessorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Yangtao.Hosting.Extensions;
+
+namespace Yangtao.Hosting.Http.Extensions
+{
+    public static class HttpContextAccessorExtensions
+    {
+        public static string GetClaimValue(this IHttpContextAccessor contextAccessor, string key)
+        {
+            if (contextAccessor == null) return string.Empty;
+            if (contextAccessor.HttpContext == null) return string.Empty;
+            if (contextAccessor.HttpContext.User == null) return string.Empty;
+
+            var claims = contextAccessor.HttpContext.User.Claims;
+            if (claims.IsNullOrEmpty()) return string.Empty;
+
+            var r = claims.FirstOrDefault(a => a.Type == key);
+            if (r == null) return string.Empty;
+
+            return r.Value;
+        }
+
+        public static string GetUserId(this IHttpContextAccessor contextAccessor) => contextAccessor.GetClaimValue("UserId");
+    }
+}
