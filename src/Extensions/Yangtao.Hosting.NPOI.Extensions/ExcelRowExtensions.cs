e165b7adb80759597845331c0bee6f3ee2f9e2d8
Yangtao Jiang <jiangyangtao1@qq.com>
2024-04-14 05:57:12 +0800

    Add ExcelRowExtensions

---
+++ src/Extensions/Yangtao.Hosting.NPOI.Extensions/ExcelRowExtensions.cs
@@ -0,0 +1,17 @@
+﻿using NPOI.SS.UserModel;
+
+namespace Yangtao.Hosting.NPOI.Extensions
+{
+    public static class ExcelRowExtensions
+    {
+        public static void AddCells(this IRow row, int cellCount)
+        {
+            for (int i = 0; i < cellCount; i++)
+            {
+                row.CreateCell(row.Cells.Count + 1);
+            }
+        }
+
+        public static bool IsAllBlank(this IRow cells) => cells.All(a => a.CellType == CellType.Blank);
+    }
+}
--- src/Extensions/Yangtao.Hosting.NPOI.Extensions/WorkbookExtensions.cs
+++ src/Extensions/Yangtao.Hosting.NPOI.Extensions/WorkbookExtensions.cs
@@ -1,7 +1,18 @@
-﻿namespace Yangtao.Hosting.NPOI.Extensions
-{
-    public class WorkbookExtensions
-    {
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
+namespace Yangtao.Hosting.NPOI.Extensions
+{
+    public static class WorkbookExtensions
+    {
+        public static string GetContentType(this IWorkbook workBook)
+        {
+            if (workBook is HSSFWorkbook hssf) return hssf.DocumentSummaryInformation.ContentType;
+
+            if (workBook is XSSFWorkbook xssf) return xssf.WorkbookType.ContentType;
+
+            return string.Empty;
+        }
     }
 }
