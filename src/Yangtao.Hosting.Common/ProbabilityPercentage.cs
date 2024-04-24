e0bb594219bfd9558c96daf1d8f8886fca58dc7d
Yangtao Jiang <jiangyangtao1@qq.com>
2024-04-24 13:35:31 +0800

    Add ProbabilityPercentage

---
+++ src/Yangtao.Hosting.Common/ProbabilityPercentage.cs
@@ -0,0 +1,28 @@
+﻿namespace Yangtao.Hosting.Common
+{
+    /// <summary>
+    /// 概率百分比
+    /// </summary>
+    public class ProbabilityPercentage
+    {
+        private readonly int Percentage;
+
+        private ProbabilityPercentage(int percentage)
+        {
+            Percentage = percentage;
+        }
+
+        public static ProbabilityPercentage Create(int percentage) => new(percentage);
+
+        public bool IsHit => RandomNumber <= Percentage;
+
+        private static int RandomNumber
+        {
+            get
+            {
+                var random = new Random();
+                return random.Next(0, 100);
+            }
+        }
+    }
+}
