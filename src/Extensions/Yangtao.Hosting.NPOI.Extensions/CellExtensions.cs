d00a47e576062c4ae046b5f4e9cac30c3b70bbc6
Yangtao Jiang <jiangyangtao1@qq.com>
2024-04-27 18:35:14 +0800

    Add CellExtensions

---
+++ src/Extensions/Yangtao.Hosting.NPOI.Extensions/CellExtensions.cs
@@ -0,0 +1,17 @@
+﻿using NPOI.SS.UserModel;
+
+namespace Yangtao.Hosting.NPOI.Extensions
+{
+    public static class CellExtensions
+    {
+        public static ICell DrawBorder(this ICell cell)
+        {
+            cell.CellStyle.BorderTop = BorderStyle.Thin;
+            cell.CellStyle.BorderRight = BorderStyle.Thin;
+            cell.CellStyle.BorderBottom = BorderStyle.Thin;
+            cell.CellStyle.BorderLeft = BorderStyle.Thin;
+
+            return cell;
+        }
+    }
+}
