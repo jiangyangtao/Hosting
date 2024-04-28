c96757936102a25846c3d362a622de4fbcd0e982
Yangtao Jiang <jiangyangtao1@qq.com>
2024-04-28 13:09:34 +0800

    Add NPOI the extention method

--- src/Extensions/Yangtao.Hosting.NPOI.Extensions/CellExtensions.cs
+++ src/Extensions/Yangtao.Hosting.NPOI.Extensions/CellExtensions.cs
@@ -1,9 +1,18 @@
 ﻿using NPOI.SS.UserModel;
+using System.Text;
 
 namespace Yangtao.Hosting.NPOI.Extensions
 {
     public static class CellExtensions
     {
+        public static ICell CreateStyleCell(this IRow row, int index, IWorkbook workBook)
+        {
+            var cell = row.CreateCell(index);
+            cell.CellStyle = workBook.CreateCellStyle();
+
+            return cell;
+        }
+
         public static ICell DrawBorder(this ICell cell)
         {
             cell.CellStyle.BorderTop = BorderStyle.Thin;
@@ -12,6 +21,47 @@ namespace Yangtao.Hosting.NPOI.Extensions
             cell.CellStyle.BorderLeft = BorderStyle.Thin;
 
             return cell;
+        }
+
+        public static ICell SetFont(this ICell cell, IFont font)
+        {
+            cell.CellStyle.SetFont(font);
+            return cell;
+        }
+
+        public static ICell SetBlodFont(this ICell cell, IWorkbook wrokbook)
+        {
+            var font = wrokbook.CreateFont();
+            font.IsBold = true;
+            cell.SetFont(font);
+
+            return cell;
+        }
+
+        public static ISheet AutoSizeColumn(this ISheet sheet)
+        {
+            var first = sheet.GetRow(0);
+            if (first == null) return sheet;
+
+            for (int col = 0; col < first.Cells.Count; col++)
+            {
+                sheet.AutoSizeColumn(col);
+                int columnWidth = sheet.GetColumnWidth(col) / 256;
+                for (int rowIndex = 0; rowIndex <= sheet.LastRowNum; rowIndex++)
+                {
+                    var row = sheet.GetRow(rowIndex);
+                    var cell = row.GetCell(col);
+
+                    var content = cell != null ? cell.ToString() : string.Empty;
+                    int contextLength = Encoding.UTF8.GetBytes(content).Length;
+                    columnWidth = columnWidth < contextLength ? contextLength : columnWidth;
+
+                    row.Height = 360;
+                }
+                sheet.SetColumnWidth(col, columnWidth * 256);
+            }
+
+            return sheet;
         }
     }
 }
