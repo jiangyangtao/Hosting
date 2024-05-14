62cfeb5c33122422549813d713a5e681fcc1820d
Yangtao Jiang <jiangyangtao1@qq.com>
2024-05-15 07:44:26 +0800

    Add XmlDocumentHandler

---
+++ src/FrontendApi/Yangtao.Hosting.FrontendApi/StaticKeys.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Yangtao.Hosting.FrontendApi.Enums;
+using Yangtao.Hosting.Mvc.FormatResult;
+using Yangtao.Hosting.Mvc.HttpMethods;
+
+namespace Yangtao.Hosting.FrontendApi
+{
+    internal class StaticKeys
+    {
+        public const string ControllerKey = "Controller";
+
+        public static readonly Type StringType = typeof(string);
+        public static readonly Type ShortType = typeof(short);
+        public static readonly Type ShortNullableType = typeof(short?);
+        public static readonly Type IntType = typeof(int);
+        public static readonly Type IntNullableType = typeof(int?);
+        public static readonly Type Int64Type = typeof(long);
+        public static readonly Type Int64NullableType = typeof(long?);
+        public static readonly Type FloatType = typeof(float);
+        public static readonly Type FloatNullableType = typeof(float?);
+        public static readonly Type DoubleType = typeof(double);
+        public static readonly Type DoubleNullableType = typeof(double?);
+        public static readonly Type DecimalType = typeof(decimal);
+        public static readonly Type DecimalNullableType = typeof(decimal?);
+        public static readonly Type DateTimeType = typeof(DateTime);
+        public static readonly Type DateTimeNullableType = typeof(DateTime?);
+        public static readonly Type BooleanType = typeof(bool);
+        public static readonly Type BooleanNullableType = typeof(bool?);
+        public static readonly Type GuidType = typeof(Guid);
+        public static readonly Type GuidNullableType = typeof(Guid?);
+
+
+        public static readonly Type PaginationType = typeof(PagingParameter);
+
+
+        public static readonly Type HttpGetType = typeof(HttpGetAttribute);
+        public static readonly Type HttpDefaultGetType = typeof(HttpGetDefaultAttribute);
+
+        public static readonly Type HttpPostType = typeof(HttpPostAttribute);
+        public static readonly Type HttpDefaultPostType = typeof(HttpPostDefaultAttribute);
+
+        public static readonly Type HttpPutType = typeof(HttpPutAttribute);
+        public static readonly Type HttpDefaultPutType = typeof(HttpPutDefaultAttribute);
+
+        public static readonly Type HttpPatchType = typeof(HttpPatchAttribute);
+        public static readonly Type HttpDefaultPatchType = typeof(HttpPatchDefaultAttribute);
+
+        public static readonly Type HttpDeleteType = typeof(HttpDeleteAttribute);
+        public static readonly Type HttpDefaultDeleteType = typeof(HttpDeleteDefaultAttribute);
+
+    }
+}
---
+++ src/FrontendApi/Yangtao.Hosting.FrontendApi/XmlDocumentHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using Yangtao.Hosting.Extensions;
+
+namespace Yangtao.Hosting.FrontendApi
+{
+    internal class XmlDocumentHandler
+    {
+        private readonly Dictionary<string, XDocument> XmlDocmentDictionary;
+
+        public XmlDocumentHandler()
+        {
+            XmlDocmentDictionary = new();
+        }
+
+        public string? GetFieldSummary(FieldInfo field)
+        {
+            var assemblyName = field.FieldType.Assembly.GetName().Name;
+            var memberName = $"F:{field.FieldType.FullName}.{field.Name}";
+            return GetSummaryText(assemblyName, memberName);
+        }
+
+        public string? GetModuleSummary(Type module)
+        {
+            var assemblyName = module.Assembly.GetName().Name;
+            var memberName = $"T:{module.FullName}";
+
+            return GetSummaryText(assemblyName, memberName);
+        }
+
+        public string? GetPropertySummary(PropertyInfo property)
+        {
+            var assemblyName = property.DeclaringType.Assembly.GetName().Name;
+            var memberName = $"P:{property.DeclaringType.FullName}.{property.Name}";
+
+            var text = GetSummaryText(assemblyName, memberName);
+            if (property.PropertyType.IsEnum && text.IsNullOrEmpty()) return GetEnumSummary(property);
+
+            return text;
+        }
+
+        public string? GetEnumSummary(PropertyInfo property)
+        {
+            var assemblyName = property.PropertyType.Assembly.GetName().Name;
+            var memberName = $"T:{property.PropertyType.FullName}";
+            return GetSummaryText(assemblyName, memberName);
+        }
+
+        private string? GetSummaryText(string assemblyName, string memberName)
+        {
+            var xmlDoc = GetXmlDocument(assemblyName);
+            if (xmlDoc == null) return string.Empty;
+
+            return xmlDoc.XPathEvaluate($"normalize-space(//member[@name = '{memberName}']/summary/text())") as string;
+        }
+
+        private XDocument? GetXmlDocument(string assemblyName)
+        {
+            if (XmlDocmentDictionary.ContainsKey(assemblyName)) return XmlDocmentDictionary[assemblyName];
+
+            var xmlFile = $"{assemblyName}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            var existPath = File.Exists(xmlPath);
+            if (existPath == false) return null;
+
+            var xmlDoc = XDocument.Load(xmlPath);
+            XmlDocmentDictionary.Add(assemblyName, xmlDoc);
+            return xmlDoc;
+        }
+    }
+}
