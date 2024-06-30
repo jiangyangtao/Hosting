1adbcc55931e26fac1578aba19ad22ad2fe0647e
Yangtao Jiang <jiangyangtao1@qq.com>
2024-06-30 08:51:52 +0800

    Add DataPagination

---
+++ src/Yangtao.Hosting.Mvc/FormatResult/DataPagination.cs
@@ -0,0 +1,27 @@
+﻿namespace Yangtao.Hosting.Mvc.FormatResult
+{
+    public class DataPagination<TData> where TData : class
+    {
+        public DataPagination()
+        {
+            List = Array.Empty<TData>();
+        }
+
+        public DataPagination(long count) : this()
+        {
+            Count = count;
+        }
+
+        public DataPagination(long count, IEnumerable<TData> list) : this(count)
+        {
+            List = list;
+        }
+
+        /// <summary>
+        /// 数据总量
+        /// </summary>
+        public long Count { get; } = 0;
+
+        public IEnumerable<TData> List { set; get; }
+    }
+}
--- src/Yangtao.Hosting.Mvc/FormatResult/PaginationResult.cs
+++ src/Yangtao.Hosting.Mvc/FormatResult/PaginationResult.cs
@@ -1,19 +1,48 @@
 ﻿
+using Yangtao.Hosting.Extensions;
+
 namespace Yangtao.Hosting.Mvc.FormatResult
 {
-    public class PaginationResult<T>
+    public class PaginationResult<TResult> where TResult : class
     {
-        public PaginationResult(long count)
+        public PaginationResult()
+        {
+            List = Array.Empty<TResult>();
+        }
+
+        public PaginationResult(long count) : this()
         {
             Total = count;
-            List = Array.Empty<T>();
+        }
+
+        public PaginationResult(long count, IEnumerable<TResult> list) : this(count)
+        {
+            List = list;
+        }
+
+        public PaginationResult(long count, Func<IEnumerable<TResult>> func) : this(count)
+        {
+            List = func();
         }
 
         /// <summary>
         /// 数据总量
         /// </summary>
-        public long Total { get; }
+        public long Total { get; } = 0;
 
-        public IEnumerable<T> List { set; get; }
+        public IEnumerable<TResult> List { set; get; }
+    }
+
+    public class PaginationResult<TResult, TData> : PaginationResult<TResult> where TResult : class where TData : class
+    {
+        public PaginationResult() : base()
+        {
+
+        }
+
+        public PaginationResult(DataPagination<TData> result, Func<TData, TResult> func) : base(result.Count)
+        {
+            if (result.List.NotNullAndEmpty()) List = result.List.Select(a => func(a));
+        }
     }
 }
