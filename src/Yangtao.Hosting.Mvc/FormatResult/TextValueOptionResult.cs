514983ee306e63d189b0463aaa75895be7086420
Yangtao Jiang <jiangyangtao1@qq.com>
2024-07-26 03:07:37 +0800

    Add TextValueOptionCollection

---
+++ src/Yangtao.Hosting.Mvc/FormatResult/TextValueOptionResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using Yangtao.Hosting.Extensions;
+
+namespace Yangtao.Hosting.Mvc.FormatResult
+{
+    public class TextValueOptionCollection : IEnumerable<TextValueOption>
+    {
+        private readonly List<TextValueOption> Options;
+
+        public TextValueOptionCollection()
+        {
+            Options = new List<TextValueOption>();
+        }
+
+        public TextValueOptionCollection(IEnumerable<TextValueOption> options) : this()
+        {
+            if (options.NotNullAndEmpty()) Options.AddRange(options);
+        }
+
+        public static IEnumerable<TextValueOption> Empty => Array.Empty<TextValueOption>();
+
+        public void Add(TextValueOption option) => Options.Add(option);
+
+        public IEnumerator<TextValueOption> GetEnumerator() => Options.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
