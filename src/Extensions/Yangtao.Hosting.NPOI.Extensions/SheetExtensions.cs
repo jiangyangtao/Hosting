2951eb0c36c665930c2ba3654378332394fea458
Yangtao Jiang <jiangyangtao1@qq.com>
2024-08-31 18:16:12 +0800

    Add SheetExtensions

---
+++ src/Extensions/Yangtao.Hosting.NPOI.Extensions/SheetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using NPOI.SS.UserModel;
+
+namespace Yangtao.Hosting.NPOI.Extensions
+{
+    public static class SheetExtensions
+    {
+        public static string[] GetColumns(this ISheet sheet)
+        {
+            var headerRow = sheet.GetRow(0);
+            var cellCount = headerRow.LastCellNum;
+            var excelcolumnNames = new List<string>();
+            for (int i = headerRow.FirstCellNum; i < cellCount; i++)
+            {
+                var cell = headerRow.GetCell(i);
+                excelcolumnNames.Add(cell.StringCellValue);
+            }
+
+            return excelcolumnNames.ToArray();
+        }
+    }
+}
