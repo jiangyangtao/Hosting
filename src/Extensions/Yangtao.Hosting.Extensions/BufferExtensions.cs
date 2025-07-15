aceda074c2860bf96f3616dd9432975a84cb178c
Yangtao Jiang <jiangyangtao1@qq.com>
2025-07-15 12:12:29 +0800

    Add StreamExtensions

---
+++ src/Extensions/Yangtao.Hosting.Extensions/BufferExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Yangtao.Hosting.Extensions
+{
+    public static class BufferExtensions
+    {
+        public static Stream ToStream(this byte[] buffer) => new MemoryStream(buffer);
+    }
+}
---
+++ src/Extensions/Yangtao.Hosting.Extensions/StreamExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Yangtao.Hosting.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ToBuffer(this Stream stream)
+        {
+            var bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+
+            stream.Seek(0, SeekOrigin.Begin);
+            return bytes;
+        }
+    }
+}
