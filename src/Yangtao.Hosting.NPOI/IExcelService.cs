844e297fc4b48c8d3bfa8d84ad85d47ebef46b0e
Yangtao Jiang <jiangyangtao1@qq.com>
2025-07-21 02:57:02 +0800

    Add ExcelService

---
+++ src/Yangtao.Hosting.NPOI/IExcelService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yangtao.Hosting.NPOI
+{
+    /// <summary>
+    /// Excel 服务
+    /// </summary>
+    public interface IExcelService
+    {
+
+    }
+}
