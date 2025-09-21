d033aef922a7831a1321c9c975b8decb47674b17
Yangtao Jiang <jiangyangtao1@qq.com>
2025-09-21 15:49:27 +0800

    Add SetAuthor

--- src/Extensions/Yangtao.Hosting.NPOI.Extensions/WorkbookExtensions.cs
+++ src/Extensions/Yangtao.Hosting.NPOI.Extensions/WorkbookExtensions.cs
@@ -1,4 +1,5 @@
-﻿using NPOI.HSSF.UserModel;
+﻿using NPOI.HPSF;
+using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
@@ -23,6 +24,48 @@ namespace Yangtao.Hosting.NPOI.Extensions
             font.Color = IndexedColors.Red.Index;
 
             return font;
+        }
+
+        private static ICellStyle CellStyle { set; get; }
+
+        public static ICellStyle GetCellStyle(this IWorkbook workbook)
+        {
+            CellStyle ??= workbook.CreateCellStyle();
+
+            return CellStyle;
+        }
+
+        public static void SetAuthor(this IWorkbook workbook, string author)
+        {
+            if (workbook is XSSFWorkbook)
+            {
+                var xssfWorkbook = workbook as XSSFWorkbook;
+                var xmlProps = xssfWorkbook.GetProperties();
+                var coreProps = xmlProps.CoreProperties;
+                coreProps.Creator = author;
+                return;
+            }
+
+            if (workbook is HSSFWorkbook)
+            {
+                var hssfWorkbook = workbook as HSSFWorkbook;
+                var summaryInfo = hssfWorkbook.SummaryInformation;
+
+                if (summaryInfo != null)
+                {
+                    summaryInfo.Author = author;
+                    return;
+                }
+
+                var newDocInfo = PropertySetFactory.CreateDocumentSummaryInformation();
+
+                var newInfo = PropertySetFactory.CreateSummaryInformation();
+                newInfo.Author = author;
+
+                hssfWorkbook.DocumentSummaryInformation = newDocInfo;
+                hssfWorkbook.SummaryInformation = newInfo;
+
+            }
         }
     }
 }
